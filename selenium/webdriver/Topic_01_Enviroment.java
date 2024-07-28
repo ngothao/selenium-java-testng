2dd7b241c6c0681c066e016dfa6fb5bc82a23da9
Ngo Thao <ngothao.tm@gmail.com>
2024-07-28 10:49:45 +0700

    Init repo

---
+++ selenium/webdriver/Topic_01_Enviroment.java
@@ -0,0 +1,25 @@
+package webdriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Topic_01_Enviroment
+{
+    WebDriver driver;
+
+    @Test
+    public void TC_01_Run_On_FireFox()
+    {
+        driver = new FirefoxDriver();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+    }
+    @Test
+    public void TC_02_Run_On_Chrome()
+    {
+        driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+    }
+}
